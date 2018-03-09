6d4f1b2c7b8781229fd6e8d7d6dcecadec7ef95a
Tiago Pinto <7tiago.pinto@gmail.com>
2018-03-09 14:50:03 +0000

    added licenses

--- src/skill_provider.py
+++ src/skill_provider.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
+#
+# Copyright 2015 Airbus
+# Copyright 2017 Fraunhofer Institute for Manufacturing Engineering and Automation (IPA)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 import os
 import sys
@@ -10,21 +26,21 @@ import importlib
 
 
 class SkillProvider(object):
-    
+
     def __init__(self, skill_register_file):
-        
+
         if not os.path.isfile(skill_register_file):
             raise Exception("Invalid register file path !")
-        
+
         self._tree = ElementTree.parse(skill_register_file)
         self._root = self._tree.getroot()
-    
+
     def load(self, name):
-        
+
         """ Find skill registered by name
         <skill name="${name}" pkg="${pkg}" module="${module}" class="${class}"/>
         """
-        
+
         skill = self._root.find('skill[@name="%s"]'%name)
         if skill is not None:
             try:
@@ -52,4 +68,3 @@ class SkillProvider(object):
         else:
             rospy.logerr('[SCXML Interpreter] Skill named "%s" not found in register file !'%name)
             return None
-
