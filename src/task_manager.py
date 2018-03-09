d7abe1702f86d9d83b2f24df992be83cd17127bd
Tiago Pinto <7tiago.pinto@gmail.com>
2018-03-09 14:47:22 +0000

    repository orgranization

--- src/task_manager.py
+++ src/task_manager.py
@@ -7,7 +7,7 @@ import smach_ros
 if __name__ == "__main__":
     rospy.init_node('task_manager')
 
-    scxml_file = open('/home/tiago/catkin_ws/src/task_manager_scxml/task_manager_scxml_server/resources/scxml/test.scxml')
+    scxml_file = open('/home/tiago/catkin_ws/src/task_manager_scxml/scxml_interpreter/resources/scxml/test.scxml')
 
     interpreter = SCXMLInterpreter(scxml_file)
     sm = interpreter.convertSCXML()
