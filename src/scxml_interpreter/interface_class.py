e91dae79a0f0d1623ea991eea8cf31c7716cf35a
Shruti Subramanian <shruti.subramanian@ipa.fraunhofer.de>
2018-07-02 15:21:38 +0200

    Changed in compound state interface class

--- src/scxml_interpreter/interface_class.py
+++ src/scxml_interpreter/interface_class.py
@@ -2,22 +2,22 @@
 
 
 class SimpleStateInterface(object):
-    def __init__(self, id, data={}, transitions={},onentry=[],onexit=[]):
+    def __init__(self, id, data={}, transitions={},onentry={},onexit={}):
         self.id = id
         self.data = data
         self.transitions = transitions
         self.onentry=onentry
         self.onexit=onexit
 
-        
+
     def get_outcomes(self):
         return list(self.transitions.keys())
 
     def __str__(self):
         return ("SimpleStateInterface(\nid=%s\ndata=%s\ntransitions=%s\nonentry=%s\nonexit=%s\n)"%(str(self.id),str(self.data),str(self.transitions),str(self.onentry),str(self.onexit)))
-        
+
 class CompoundStateInterface(object):
-    def __init__(self, id, data={}, transitions={}, states=[], initial_state_id="",onentry=[],onexit=[]):
+    def __init__(self, id, data={}, transitions={}, states=[], initial_state_id="",onentry={},onexit={}):
         self.id = id
         self.data = data
         self.transitions = transitions
@@ -28,12 +28,12 @@ class CompoundStateInterface(object):
 
     def get_outcomes(self):
         return list(self.transitions.keys())
-    
+
     def get_outcome_target(self, outcome):
         if(outcome in self.transitions):
             return self.transitions[outcome]
         else:
-            return None ##TODO return proper error code      
+            return None ##TODO return proper error code
     def __str__(self):
         state_print = ""
         for state in self.states:
@@ -86,7 +86,7 @@ class RootStateInterface(object):
         self.data = data
         self.final_states = final_states
         self.states = states
-        
+
     def __str__(self):
         state_print = ""
         for state in self.states:
@@ -98,16 +98,16 @@ class RootStateInterface(object):
         return ("RootStateInterface(\ninitial_state_id=%s\ndata=%s\nfinal_states=%s\nstates=[\n%s])"%(
             str(self.initial_state_id),str(self.data),str(self.final_states),str(state_print))
         )
-        
+
 class SCXMLInterface(object):
     def __init__(self):
         self.simpleStates = []
         self.compoundStates = []
         self.rootState = None
-        
+
     def debug_print(self):
         return str(self)
-        
+
     def __str__(self):
         str_print = ""
         for state in self.simpleStates:
@@ -116,6 +116,3 @@ class SCXMLInterface(object):
             str_print = str_print + str(state) + "\n"
         str_print = str_print + str(self.rootState)
         return str_print
-            
-        
-        
